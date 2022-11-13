4a7dcf4c9f44e8c7cf6b8fd9c642c6027df0c7ba
maryamkaramad <maryamkaramad1373@gmail.com>
2022-11-13 15:50:18 +0330

    create moneyManegmant

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import MonyCart from '../componens/Home/MonyCart'
 import Total from '../componens/Home/Total'
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import Chart from '../componens/Home/Chart';
 
 
 
@@ -19,6 +20,7 @@ const Home = (): JSX.Element => {
             <Total />
             <MonyCart backgroundcolor={"success.main"} title={"هزینه ها"} icon={<TrendingUpIcon />} type={"income"} />
             <MonyCart backgroundcolor={"error.main"} title={"خرج ها"} icon={<TrendingDownIcon />} type={"expense"} />
+            <Chart />
         </Grid>
     )
 }
